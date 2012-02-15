c87069bb8e712ef1545c29c31481bdbd61bbc63d
Marko Alas <markoa@gmail.com>
2012-02-15 22:05:39 +0200

    renamed methods in Topic

--- app/controllers/Topic.java
+++ app/controllers/Topic.java
@@ -5,15 +5,15 @@ import play.data.validation.Required;
 import play.data.validation.Validation;
 
 public class Topic extends SecureController {
-    public static void addTopic() {
+    public static void add() {
         render();
     }
 
-    public static void saveTopic(@Required @MaxSize(255) String topicTitle, String description, @MaxSize(255) String proposedSpeaker) {
+    public static void save(@Required @MaxSize(255) String topicTitle, String description, @MaxSize(255) String proposedSpeaker) {
         if (Validation.hasErrors()) {
             params.flash();
             Validation.keep();
-            addTopic();
+            add();
         }
 
         new models.Topic(getConnectedUser(), topicTitle, description, proposedSpeaker).save();
